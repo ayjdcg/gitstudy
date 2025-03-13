8cfc922a71a7673e329e582c2411c6f48af24fbd
cuizhiguo <76873334+ayjdcg@users.noreply.github.com>
2025-03-13 11:36:43 +0800

    分支第2次提交

--- src/main/java/org/czg/App.java
+++ src/main/java/org/czg/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "123456" );
-        System.out.println("分支1提交");
+        System.out.println("分支1次提交");
+        System.out.println("分支2次提交");
     }
 }
