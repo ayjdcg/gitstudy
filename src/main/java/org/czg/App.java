93e13e74c44be2cf6daa27a7845999286d390e92
cuizhiguo <76873334+ayjdcg@users.noreply.github.com>
2025-03-13 14:56:40 +0800

    回归主线

--- src/main/java/org/czg/App.java
+++ src/main/java/org/czg/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "123456" );
-        System.out.println("分支1次提交");
-        System.out.println("分支2次提交");
+        System.out.println("回归主线");
     }
 }
