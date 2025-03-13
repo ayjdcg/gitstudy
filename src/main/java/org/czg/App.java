2d3ebb5eefb40adb6017092a147e495d8c0df6a9
cuizhiguo <76873334+ayjdcg@users.noreply.github.com>
2025-03-13 11:33:36 +0800

    第一次提交

--- src/main/java/org/czg/App.java
+++ src/main/java/org/czg/App.java
@@ -4,10 +4,10 @@ package org.czg;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "123456" );
     }
 }
